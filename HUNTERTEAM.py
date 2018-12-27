04914619db203b63413b3940812c98a3375a2190
hunterteam6734 <46200646+hunterteam6734@users.noreply.github.com>
2018-12-27 20:48:49 +0200

    Update and rename README.md to HUNTERTEAM.py

---
+++ HUNTERTEAM.py
@@ -0,0 +1,60 @@
+# -*- coding: cp1254 -*-
+
+
+
+kuladi = "recep"
+sifresi = "recep"
+kullanici_adi=input("Kullanıcı adınızı giriniz : ")
+sifre=input("Sifrenizi giriniz : ")
+if kullanici_adi==kuladi and sifre==sifresi:
+    print("Giriş Başarılı...")
+elif kullanici_adi!=kuladi and sifre!=sifresi:
+    print("Girdiğiniz Bilgileri Kontrol Ediniz.")
+    quit()
+else:
+    print("Giriş Başarısız...")
+    quit()
+print("HUNTERTEAM için yapılmış bir yazılımdır...")
+print("""
+	_   _ _   _ _   _ _____ _____ ____ _____ _____    _    __  __
+| | | | | | | \ | |_   _| ____|  _ \_   _| ____|  / \  |  \/  |
+| |_| | | | |  \| | | | |  _| | |_) || | |  _|   / _ \ | |\/| |
+|  _  | |_| | |\  | | | | |___|  _ < | | | |___ / ___ \| |  | |
+|_| |_|\___/|_| \_| |_| |_____|_| \_\|_| |_____/_/   \_\_|  |_|
+""")
+print("HUNTERTEAM in hack aracıdır")
+print("""
+0 - ÇIKIŞ
+1 - BİLGİ
+2 - SOSYAL MÜHENDİSLİK
+3 - WORDLİSS/PASSLİST
+4 - SUA
+5 - TERMUX
+6 - GİTHUB
+
+""")
+while True:
+    scanner = input("HUNTER ne yapmak istersin:")
+
+    if scanner == "0":
+        print("kapat")
+        break
+    scanner
+    if scanner == "1":
+        print("Bu program HUNTERTEAM in 1.seviyesidir 2.seviye için daha çok çalışmalısın.\n 2.seviyeyi hak ettiğinde sana verilecek.Ve unutma HUNTERTEAM 3 seviyeden oluşur.")
+    scanner 
+    if scanner == "2":
+        print("sosyal mühendislik için site ve hesap:\n Z-SHADOW\n hesap:\n kullanıcı adı:HUNTERTEAM6734\n şifre :hunterteam\n")
+    scanner
+    if scanner == "3":
+        print("wordpress için oluşturulmuş wordlist ve passlist.\n burdan indirebilirsiniz.\n https://drive.google.com/open?id=1-GIqO0OsygaVngv7MvRM_aIceCNXVqkc")
+    scanner
+    if scanner == "4":
+        print("Sua yerli yazılım dili C# altyapısını kullanır.\nİndirmek için http://sua.gen.tr/indirmeler.html.\nÖğrenmek için http://sua.gen.tr/ogren/. ")
+    scanner
+    if scanner == "5":
+        print("TERMUX tamamen kali linux üzerine kurulu bir sistemdir.Burda sizlerle TERMUX öğrenicez ve sizlere HUNTERTEAM e özel paketlerde paylaşıcam")
+    scanner
+    if scanner == "6":
+        print("GİTHUB hesabı:\nhesabın adı:hunterteam6734\nhesabın e mail i:hunterteam6734@gmail.com\nhesabın şifresi:12345a1B2c3D4")
+
